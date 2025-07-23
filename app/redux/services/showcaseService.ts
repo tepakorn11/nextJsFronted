5a2fddac0bda2a571651a1a02cbc3c61fb41f997
90q <58302466+tepakorn11@users.noreply.github.com>
2025-07-23 17:48:45 +0700

    เพิ่มส่งที่ status active ไป

--- app/redux/services/showcaseService.ts
+++ app/redux/services/showcaseService.ts
@@ -2,6 +2,11 @@ import { IShowCase } from "@/interface/showcase";
 import ax from "@/utility/axios"
 
 export const getShowCase = async (): Promise<IShowCase[]> => {
-    const res = await ax.get<IShowCase[]>('/show-case')
+    const res = await ax.get<IShowCase[]>('/show-case',{
+        params: {
+            status:'active',
+        }
+    })
     return res.data;
 }
+
